aedf53c4dd0ef994d67c8288fe4dac00e5422549
Kimberly Rodriguez <kimmiethecoder@gmail.com>
2022-11-24 03:28:04 -0500

    basicJS through augmented operators-20percent

---
+++ main.js
@@ -0,0 +1,31 @@
+// Only change code below this line
+var a;
+a = 5;
+var b;
+b = 10;
+var c;
+c = "I am a";
+// Only change code above this line
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+// Operators
+let myVar = 10 
+
+// add 1
+myVar++
+
+// subtract 1
+myVar--
+
+// Compound assignment with augmented subtraction
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
